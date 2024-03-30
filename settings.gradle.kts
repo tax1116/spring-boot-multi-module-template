eed2bd39de0da7eea9d13fccd186d1ae88c049cf
tax1116 <tax941116@gmail.com>
2024-03-31 00:41:57 +0900

    Feat : Add Demo modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+// TODO: Change the root project name
+rootProject.name = "spring-boot-multi-module-template"
 
 include(
+    "demo", // TODO: Change the name of the project
     // TODO: Add the names of the projects to include
 )
